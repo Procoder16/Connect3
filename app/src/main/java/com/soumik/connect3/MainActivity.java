3258e9769c0b15f875a03bbfb782cf04df1be7d5
Soumik Mukherjee <smsoumik1608@gmail.com>
2020-09-30 21:54:04 +0530

    New Releases and Bug Fixes

--- app/src/main/java/com/soumik/connect3/MainActivity.java
+++ app/src/main/java/com/soumik/connect3/MainActivity.java
@@ -3,8 +3,18 @@ package com.soumik.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        ImageView counter=(ImageView)view;
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
