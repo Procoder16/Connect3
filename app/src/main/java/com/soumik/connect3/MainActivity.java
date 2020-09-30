701f3aaf6f6704b08dd480264a6b7cb5f5dd2cac
Soumik Mukherjee <smsoumik1608@gmail.com>
2020-09-30 22:05:19 +0530

    New Releases and Bug Fixes

--- app/src/main/java/com/soumik/connect3/MainActivity.java
+++ app/src/main/java/com/soumik/connect3/MainActivity.java
@@ -8,11 +8,22 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0=yellow,1=red
+
+    int activePlayer=0;
+
     public void dropIn(View view){
 
         ImageView counter=(ImageView)view;
         counter.setTranslationY(-1500);
+        if(activePlayer==0){
         counter.setImageResource(R.drawable.yellow);
+        activePlayer=1;
+        }
+        else{
+            counter.setImageResource(R.drawable.red);
+            activePlayer=0;
+        }
         counter.animate().translationYBy(1500).setDuration(300);
     }
 
