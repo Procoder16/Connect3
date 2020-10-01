65443c65e039b96242c597902dacf7efe63a35d9
Soumik Mukherjee <smsoumik1608@gmail.com>
2020-10-02 00:39:44 +0530

    New Releases and Bug Fixes

--- app/src/main/java/com/soumik/connect3/MainActivity.java
+++ app/src/main/java/com/soumik/connect3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer=0;
     int[] gameState={2,2,2,2,2,2,2,2,2};
-    int[][] winningPositions={{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}};
+    int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{2,4,6},{0,4,8}};
 
     public void dropIn(View view){
 
@@ -28,7 +29,21 @@ public class MainActivity extends AppCompatActivity {
             counter.setImageResource(R.drawable.red);
             activePlayer=0;
         }
-        counter.animate().translationYBy(1500).setDuration(300);
+        counter.animate().translationYBy(1500).rotation(1800).setDuration(600);
+
+        for(int winningPosition[]:winningPositions){
+            if(gameState[winningPosition[0]]==gameState[winningPosition[1]]&&gameState[winningPosition[1]]==gameState[winningPosition[2]]&&gameState[winningPosition[0]]!=2){
+                //Someone has won!!!!
+                String winner;
+                if(activePlayer==1){
+                    winner="Yellow";
+                }
+                else{
+                    winner="Red";
+                }
+                Toast.makeText(this, winner + " has won!!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
